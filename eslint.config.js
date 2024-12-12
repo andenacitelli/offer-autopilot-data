2472ac8f15f835b12a8d28430d869025ccf7e8b0
Kal <a.acitelli@gmail.com>
2024-12-11 23:01:25 -0600

    Get Tooling Stable (#2)

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,25 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+const EXTENSION_PATTERNS = {
+  JS: ["js"].map((ext) => `**/*.${ext}`),
+  TS: ["ts"].map((ext) => `**/*.${ext}`),
+};
+
 /** @type {import("typescript-eslint").Config} */
 const config = [
-  eslint.configs.recommended,
-  tseslint.configs.strictTypeChecked,
   {
+    ...eslint.configs.recommended,
+    files: [...EXTENSION_PATTERNS.JS, ...EXTENSION_PATTERNS.TS],
+  },
+  ...tseslint.configs.recommendedTypeChecked.map((config) => {
+    return {
+      ...config,
+      files: [EXTENSION_PATTERNS.TS],
+    };
+  }),
+  {
+    files: [...EXTENSION_PATTERNS.TS],
     languageOptions: {
       parserOptions: {
         projectService: true,
