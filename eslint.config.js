2664cced65392e513dbc0e9e5f47fc2d20cea9a7
Anden Acitelli <a.acitelli@gmail.com>
2024-12-11 22:47:52 -0600

    .

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,24 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+const EXTENSION_PATTERNS = {
+  JS: ["js"].map((ext) => `**/*.${ext}`),
+  TS: ["ts"].map((ext) => `**/*.${ext}`),
+};
 /** @type {import("typescript-eslint").Config} */
 const config = [
-  eslint.configs.recommended,
-  tseslint.configs.strictTypeChecked,
   {
+    ...eslint.configs.recommended,
+    files: [...EXTENSION_PATTERNS.JS, ...EXTENSION_PATTERNS.TS],
+  },
+  ...tseslint.configs.strictTypeChecked.map((config) => {
+    return {
+      ...config,
+      files: [EXTENSION_PATTERNS.TS],
+    };
+  }),
+  {
+    files: [...EXTENSION_PATTERNS.TS],
     languageOptions: {
       parserOptions: {
         projectService: true,
